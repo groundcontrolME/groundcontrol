b2d2343633eb3f4b13809af5a9225706e24abb36
fernandosanchezmunoz <fernandosanchezmunoz@gmail.com>
2017-04-29 00:28:51 -0400

    wip

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-docker run -p 1234:80 -p 12345:443 -p 12344:5500 -v cert:/cert fernandosanchez/appstudio:groundcontrol
+docker run -p 10080:80 -p 10443:443 -p 15500:5500 -v cert:/cert fernandosanchez/appstudio:groundcontrol
